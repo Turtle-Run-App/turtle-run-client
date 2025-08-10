2568de8b82c9293049042104deef1e205293d6d8
junhcha <quffl9612@naver.com>
2025-08-10 18:42:19 +0900

    feat: add location manager

---
+++ turtle-run-client/Core/Services/LocationManager.swift
@@ -0,0 +1,86 @@
+import Foundation
+import CoreLocation
+import MapKit
+
+class LocationManager: NSObject, ObservableObject {
+    static let shared = LocationManager()
+    
+    private let locationManager = CLLocationManager()
+    
+    @Published var currentLocation: CLLocation?
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    @Published var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.5665, longitude: 126.9780), // 서울 기본 위치
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    )
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 1.0 // 1m마다 업데이트
+    }
+    
+    func requestLocationPermission() {
+        switch authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .denied, .restricted:
+            // 설정으로 이동하도록 안내
+            print("위치 권한이 거부되었습니다. 설정에서 권한을 허용해주세요.")
+        case .authorizedWhenInUse, .authorizedAlways:
+            startLocationUpdates()
+        @unknown default:
+            break
+        }
+    }
+    
+    private func startLocationUpdates() {
+        guard locationManager.authorizationStatus == .authorizedWhenInUse || 
+              locationManager.authorizationStatus == .authorizedAlways else {
+            return
+        }
+        
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopLocationUpdates() {
+        locationManager.stopUpdatingLocation()
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        DispatchQueue.main.async {
+            self.currentLocation = location
+            self.region = MKCoordinateRegion(
+                center: location.coordinate,
+                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            )
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("위치 업데이트 실패: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        DispatchQueue.main.async {
+            self.authorizationStatus = status
+        }
+        
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            startLocationUpdates()
+        case .denied, .restricted:
+            stopLocationUpdates()
+        case .notDetermined:
+            break
+        @unknown default:
+            break
+        }
+    }
+}
